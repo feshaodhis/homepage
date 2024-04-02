69eca14cf8cadc246011c3d8b85dd9f029e06bcc
fesha <feshaodhis@gmail.com>
2024-04-02 19:08:27 +0300

    connected the frontend to firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'loginScreen.dart'; // Import LoginScreen class
 import 'orderScreen.dart'; // Import OrderScreen class
@@ -5,7 +6,17 @@ import "mpesaScreen.dart";//Import MpesaScreen class
 import 'confirmationScreen.dart';
 import 'pickScreen.dart';
 
-void main() {
+Future main()  async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey: 'AIzaSyDo7Kqw5ommK7mCHtOhmCRq8aAs7aBzmZ0',
+      appId: '1:602210370636:android:5b65e2fc3bf0a166b460e9',
+      messagingSenderId: '602210370636',
+      projectId: 'utibu-health-ff94d',
+      //storageBucket: 'myapp-b9yt18.appspot.com',
+    )
+  );
   runApp(MyApp());
 }
 
